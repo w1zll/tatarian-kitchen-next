e45624ab11091ccfa48f7cec91b57b73bf9cfb25
w1zll <filin.vitalik2244@mail.ru>
2025-08-19 20:43:24 +0500

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,21 @@
-import { getToken } from 'next-auth/jwt';
+import { getToken, GetTokenParams } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
    const { pathname } = request.nextUrl;
-   const token = await getToken({
+
+   let params: GetTokenParams = {
       req: request,
-      secret: process.env.AUTH_SECRET,
-   });
+      secret: process.env.AUTH_SECRET ?? 'secret',
+   };
+
+   if (process.env.NODE_ENV === 'production') {
+      params = {
+         ...params,
+         cookieName: '__Secure-authjs.session-token',
+      };
+   }
+   const token = await getToken(params);
    const protectedRoutes = ['/ingredients', '/recipes/new', '/recipes/:path*'];
 
    if (
