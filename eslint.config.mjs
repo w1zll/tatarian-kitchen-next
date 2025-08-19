047e2d2a115eaae35b6f6973ab6eb258141c23a8
w1zll <filin.vitalik2244@mail.ru>
2025-08-19 20:21:47 +0500

    update config 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
    ...compat.extends('next/core-web-vitals', 'next/typescript'),
    {
-      ignores: ['src/generated/**'],
+      ignores: ['src/generated/**', 'src/**'],
    },
 ];
 
